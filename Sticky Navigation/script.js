fd123db965d3e25a71c696a86d675c45928db575
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-05-07 18:20:40 +0330

    finished

---
+++ Sticky Navigation/script.js
@@ -0,0 +1,11 @@
+const navs = document.querySelector('.nav');
+
+window.addEventListener('scroll', fixedNav)
+
+function fixedNav() {
+    if (window.scrollY> navs.offsetHeight + 150) {
+        navs.classList.add('active');
+    } else{
+        navs.classList.remove('active');
+    }
+}
